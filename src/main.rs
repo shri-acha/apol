95d40d88ee278baeea205e406feb35eeef5794cc
shri-acha <happy.irhs@gmail.com>
2025-11-13 22:56:41 +0545

    todo: complete parser implementation

--- src/main.rs
+++ src/main.rs
@@ -12,22 +12,29 @@ fn main() {
     loop {
         print!("\r\n>");
         io::stdout().flush().unwrap();
+        
+
         for raw_source in io::stdin().lines(){
-            let source = String::from(raw_source.unwrap_or(String::from("\n")));
 
             let mut tokens: Vec::<Token> = vec![];
+
+            let source = String::from(raw_source.unwrap_or(String::from("\n")));
+
             tokens.push(Token::new(TokenType::BOL,String::from("BOL"))); // random value for lit
             let mut lexer = Lexer::new(source);
 
             loop {
+                // looping because of the tokens are generated lazily
                 if let Some(token) = lexer.next(){
                     println!("{:?}",token);
                     tokens.push(token.clone());
                     if token.tok_type == TokenType::EOL {
                         break;
                     }
-                } 
+                }
             }
+            let mut parser = Parser::new(tokens.clone());
+            parser.parse_line();
         }
     }
 } 
--- src/parser.rs
+++ src/parser.rs
@@ -1,14 +1,79 @@
-use crate::token::{Token};
+use crate::token::{Token,TokenType};
+use std::vec::IntoIter;
 
+
+#[derive(Debug)]
+pub struct Tree<'a> {
+    pub l_child: Option<Node<'a>>,
+    pub r_child: Option<Node<'a>>,
+}
+
+impl<'a> Tree<'a> {
+    pub fn default()->Self{
+        Self{
+            l_child:None,
+            r_child:None,
+        }
+    }
+
+    pub fn new(l_child:Option<Node<'a>>,r_child: Option<Node<'a>>)->Self{
+        Self{
+            l_child,
+            r_child
+        }
+    }
+}
+#[derive(Debug)]
+pub struct Node<'a> {
+    pub value: &'a Token,
+    pub branch: Box<Tree<'a>>
+}
+
+impl<'a> Node<'a> {
+    pub fn new(tok_val: &'a Token, branch: Box<Tree<'a>>)->Self{
+        Self{
+            value: tok_val,
+            branch
+        }
+    }
+}
+
+#[derive(Debug)]
 pub struct Parser<'a> {
-    pub tok_stream: Vec<Token>,
+    pub tok_stream: IntoIter<Token>,
     pub curr_token: Option<&'a Token>
 }
+
 impl Parser<'_> {
     pub fn new( tok_stream: Vec< Token> )->Self{
         Self{
-            tok_stream,
+            tok_stream: tok_stream.into_iter(),
             curr_token:None,
         }
+    }
+    //   +
+    // 2   3
+    //  |||
+    //  |||
+    // 2 + 3
+    //
+    // cock lol
+    //
+    // TODO implement a simple AST generation 
+    
+    pub fn parse_line(&mut self)-> (){
+        if let Some(curr_token) = self.curr_token { 
+            if curr_token.tok_type == TokenType::BOL {
+                let AST = Node::new(
+                    curr_token,
+                    Box::new(Tree::new(self.consume_exprs(),self.consume_exprs()))
+                    );
+                println!("{:?}",AST);
+            }
+        }
+    }
+    pub fn consume_exprs<'a> (&mut self)-> Option<Node<'a>>{
+        println!("{:?}",self.tok_stream);
+        None
     }
 }
